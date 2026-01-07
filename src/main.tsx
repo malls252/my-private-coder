9d02d96928d29d766c36708ff7dfc1326974588f
dwihikmal <tsaqifulilmi56@gmail.com>
2026-01-07 18:26:30 +0700

    add the Workmanager

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+// Register Service Worker
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("New content available. Reload?")) {
+            updateSW(true);
+        }
+    },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === "development" && componentTagger(),
     VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico"],
       manifest: {
